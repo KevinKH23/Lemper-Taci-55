584a951894038b5711d724cea245323cd3219b2a
Kevin <kevinkurniawan265@gmail.com>
2024-09-06 15:42:36 +0700

    update until contact section

--- js/script.js
+++ js/script.js
@@ -15,3 +15,35 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// === Typing Animation ===
+var typed = new Typed("#typing-text", {
+  strings: ["Nikmat", "Gurih", "dan Lezat!"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Mendapatkan elemen
+const ctaButton = document.querySelector(".cta");
+const popup = document.getElementById("popup-menu");
+const closeBtn = document.querySelector(".popup .close");
+
+// Menampilkan pop-up saat tombol "Pesan Sekarang" diklik
+ctaButton.addEventListener("click", function (e) {
+  e.preventDefault(); // Mencegah perilaku default link
+  popup.style.display = "block"; // Menampilkan pop-up
+});
+
+// Menyembunyikan pop-up saat tombol close (x) diklik
+closeBtn.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+
+// Menyembunyikan pop-up jika pengguna mengklik di luar konten pop-up
+window.addEventListener("click", function (event) {
+  if (event.target === popup) {
+    popup.style.display = "none";
+  }
+});
